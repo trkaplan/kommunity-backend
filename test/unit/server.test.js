c0d9a42072184af071f5ebb7963aae13321ba166
Selman K <selmankahya@hotmail.com>
2018-12-02 17:12:46 -0800

    updating test

--- test/unit/server.test.js
+++ test/unit/server.test.js
@@ -1,6 +1,19 @@
+/* global jest */
 import http from 'http';
+import SequelizeMock from 'sequelize-mock';
 
 const app = require('$/server');
+
+const mockSequelize = new SequelizeMock();
+const mockModels = {
+
+};
+
+jest.mock('../../src/lib/db-client', () => ({
+  __esModule: true, // this property makes it work
+  default: () => mockSequelize,
+  importModels: () => mockModels,
+}));
 
 test('server returns 404', (done) => {
   http.get('http://localhost:4008/unknown-route', (res) => {
