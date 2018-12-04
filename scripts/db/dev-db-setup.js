48ac9b7860dfa0e3617ea4ca2f884b304576f8a2
Selman K <selmankahya@hotmail.com>
2018-12-04 00:08:41 -0800

    adding release runner

--- scripts/db/dev-db-setup.js
+++ scripts/db/dev-db-setup.js
@@ -3,15 +3,22 @@ import path from 'path';
 import Sequelize from 'sequelize';
 import DbClient, { importModels } from '$/lib/db-client';
 
-const dbClient: Sequelize = DbClient(process.env.DATABASE_URL);
-const modelsPath = path.join(__dirname, '../../src/models');
-importModels(modelsPath, dbClient);
+if (process.env.NODE_ENV === 'development') {
+  const dbClient: Sequelize = DbClient(process.env.DATABASE_URL);
+  const modelsPath = path.join(__dirname, '../../src/models');
+  importModels(modelsPath, dbClient);
 
-dbClient.sync({ force: true, match: /-development$/ }).then(() => {
-  console.log('\n>>> DB TABLE SETUP IS COMPLETED\n');
-  process.exit();
-}).catch((err) => {
-  console.log('\n>>> DB TABLE SETUP FAILED!!!\n');
-  console.log(err);
-  process.exit(1);
-});
+  dbClient.sync({
+    force: true,
+    match: /-development$/
+  })
+    .then(() => {
+      console.log('\n>>> DB TABLE SETUP IS COMPLETED\n');
+      process.exit();
+    })
+    .catch((err) => {
+      console.log('\n>>> DB TABLE SETUP FAILED!!!\n');
+      console.log(err);
+      process.exit(1);
+    });
+}
