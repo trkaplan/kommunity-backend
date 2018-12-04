01dc48355e500596c30b5e8f6028f35f76f7b1c6
Selman K <selmankahya@hotmail.com>
2018-12-04 01:24:23 -0800

    updating db setup script

--- scripts/db/mock-setup.js
+++ scripts/db/mock-setup.js
@@ -3,15 +3,14 @@ import path from 'path';
 import Sequelize from 'sequelize';
 import DbClient, { importModels } from '$/lib/db-client';
 
-if (process.env.NODE_ENV === 'development') {
+if (['development', 'staging'].indexOf(process.env.NODE_ENV) > -1) {
   const dbClient: Sequelize = DbClient(process.env.DATABASE_URL);
   const modelsPath = path.join(__dirname, '../../src/models');
   importModels(modelsPath, dbClient);
 
-  dbClient.sync({
-    force: true,
-    match: /-development$/
-  })
+  console.log('\n>>> CREATING DB TABLES\n');
+
+  dbClient.sync({ force: true })
     .then(() => {
       console.log('\n>>> DB TABLE SETUP IS COMPLETED\n');
       process.exit();
